ad115c31029e5d9521af2dec563bf65a2226ab89
novy <45167857+novysodope@users.noreply.github.com>
2021-08-02 16:06:18 +0800

    Create User.java

---
+++ test123/src/main/java/fastjsonvul/User.java
@@ -0,0 +1,20 @@
+package fastjsonvul;
+
+public class User {
+    public String name;
+    public String Age;
+
+    public void setName(String s) {
+        this.name=s;
+    }
+
+
+    public String getName() {
+        return name;
+
+    }
+
+    public void setAge(String s) {
+        this.Age = s;
+    }
+}
